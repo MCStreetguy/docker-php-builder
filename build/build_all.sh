56ffcc90cb3e091eda29d57750780fb6850d7d51
Maximilian Schmidt <m.schmidt@obis-concept.de>
2020-02-25 10:38:53 +0100

    FIX

--- build/build_all.sh
+++ build/build_all.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-declare -a IMAGES=()
 set -e
+PWD=$(pwd)
+BUILD_DIR=$(realpath $(dirname $0))
 
-while read ALPINE_VERSION; do
-  echo "Building on alpine v$ALPINE_VERSION..."
+# If working directory and build folder differ, CD there for paths to work
+if [ "$PWD" != "$BUILD_DIR" ]; then
+  unset CDPATH
+  cd $BUILD_DIR
+fi
+
+# Read all version combinations and build them
+while read _VERSION; do
+  VERSIONS=(${_VERSION//;/ })
+  ALPINE_VERSION="${VERSIONS[1]}"
+  PHP_VERSION="${VERSIONS[0]}"
+
   export ALPINE_VERSION
+  export PHP_VERSION
+  echo "Building for PHP v$PHP_VERSION on alpine v$ALPINE_VERSION..." >&2
 
-  while read PHP_VERSION; do
-    echo "Building for PHP v$PHP_VERSION"
-    export PHP_VERSION
+  TAG_NAME="mcstreetguy/php-builder:$PHP_VERSION"
+  if [ "$ALPINE_VERSION" != "latest" ]; then
+    TAG_NAME="$TAG_NAME-alpine$ALPINE_VERSION"
+  fi
 
-    TAG_NAME="mcstreetguy/php-builder:$PHP_VERSION"
-    if [ "$ALPINE_VERSION" != "latest" ]; then
-      TAG_NAME="$TAG_NAME-alpine$ALPINE_VERSION"
-    fi
+  echo "+ docker build --tag "$TAG_NAME" --build-arg ALPINE_VERSION --build-arg PHP_VERSION --compress --no-cache --no-cache .." >&2
+  docker build --tag "$TAG_NAME" --build-arg ALPINE_VERSION --build-arg PHP_VERSION --compress --no-cache --no-cache .. >&2
 
-    echo "+ docker build --tag "$TAG_NAME" --build-arg ALPINE_VERSION --build-arg PHP_VERSION --compress --no-cache --no-cache .." >&2
-    docker build --tag "$TAG_NAME" --build-arg ALPINE_VERSION --build-arg PHP_VERSION --compress --no-cache --no-cache ..
+  echo "$TAG_NAME" # Print tag name to stdout to allow piping the results
 
-    IMAGES+=( "$TAG_NAME" )
-
-    unset PHP_VERSION
-  done <targets/php.txt
+  unset PHP_VERSION
 
   unset ALPINE_VERSION
-done <targets/alpine.txt
+done <targets.txt
 
-echo "Done. Built the following images:"
-
+cd $PWD
+echo "Done." >&2
+exit 0
