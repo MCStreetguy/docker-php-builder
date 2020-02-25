2cd3c5204282ee9e1a96c19168872818b4ebfda9
Maximilian Schmidt <m.schmidt@obis-concept.de>
2020-02-25 09:23:38 +0100

    WIP

--- build/build_all.sh
+++ build/build_all.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+declare -a IMAGES=()
+set -e
+
+while read ALPINE_VERSION; do
+  echo "Building on alpine v$ALPINE_VERSION..."
+  export ALPINE_VERSION
+
+  while read PHP_VERSION; do
+    echo "Building for PHP v$PHP_VERSION"
+    export PHP_VERSION
+
+    TAG_NAME="mcstreetguy/php-builder:$PHP_VERSION"
+    if [ "$ALPINE_VERSION" != "latest" ]; then
+      TAG_NAME="$TAG_NAME-alpine$ALPINE_VERSION"
+    fi
+
+    echo "+ docker build --tag "$TAG_NAME" --build-arg ALPINE_VERSION --build-arg PHP_VERSION --compress --no-cache --no-cache .." >&2
+    docker build --tag "$TAG_NAME" --build-arg ALPINE_VERSION --build-arg PHP_VERSION --compress --no-cache --no-cache ..
+
+    IMAGES+=( "$TAG_NAME" )
+
+    unset PHP_VERSION
+  done <targets/php.txt
+
+  unset ALPINE_VERSION
+done <targets/alpine.txt
+
+echo "Done. Built the following images:"
+
